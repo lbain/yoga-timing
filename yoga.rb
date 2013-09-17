304c6e0b6862f859f5c77d12a42dcd93aa920246
Lucy Bain <lucybain@gmail.com>
2013-09-17 19:59:32 +1000

    pause on space, resume on any key, quit on q

--- yoga.rb
+++ yoga.rb
@@ -1,4 +1,26 @@
 require_relative 'sequence'
+require 'io/console'
+
+def char_if_pressed
+  system("stty raw -echo")
+  char = STDIN.read_nonblock(1) rescue nil
+  system("stty -raw echo")
+  char
+end
+
+def listen_for_pause(delay)
+  for i in 0..delay*10
+    c = char_if_pressed
+    if c
+      if /\s/ =~ c
+        STDIN.getch
+      elsif /\q/ =~ c
+        exit
+      end
+    end
+    sleep(0.1)
+  end
+end
 
 # what the user actually sees/here for each move
 def give_move(move, side, delay)
@@ -8,7 +30,7 @@ def give_move(move, side, delay)
   delay -= elapsed
   puts "#{move} #{side}"
   puts "  hold for #{delay} seconds"
-  sleep(delay)
+  listen_for_pause(delay)
 end
 
 # basically the main() function
@@ -26,12 +48,7 @@ end
 file_number = $stdin.gets.chomp.to_i
 file = file_options[file_number - 1]
 
-#TODO: or max num of rounds
 puts "What's the shortest length of time you want to hold a pose (in seconds)?"
 min_move_time = $stdin.gets.chomp.to_i
-
-puts "total_time = #{total_time}"
-puts "file = #{file}"
-puts "min_move_time = #{min_move_time}"
 
 Sequence.new('./sequences/' + file + '.rb', total_time, min_move_time)
